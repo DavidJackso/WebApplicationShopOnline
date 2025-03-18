d18d716323001c9e2d85e87710f12958bb10cc3c
Joseph <gutnov3643@gmail.com>
2025-03-18 10:42:47 +0300

    MVC User

---
+++ WebApplicationShopOnline/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index(string name, string email, string password)
+        {
+            if (name != "" || email == "" || password == "")
+            {
+                return View(new User("Ну введи имя", "И почту", "Пароль можно мне на почту"));
+            }
+            else
+            {
+               return View(new User(name,email,password));
+            }
+        }
+    }
+}
---
+++ WebApplicationShopOnline/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WebApplicationShopOnline.Models
+{
+    public class User
+    {
+        public Guid Id { get; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public User(string name,string email,string password)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+        public override string ToString()
+        {
+            return $"{Id},{Name},{Email},{Password}";
+        }
+    }
+}
